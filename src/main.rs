f6d9fb65ecbaedc2c91def07e2331f9d758e8a00
Michael Brennan <micha@DESKTOP-POQ1R6E>
2022-11-08 22:31:45 -0700

    Fuck imgui! egui supremacy 

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,19 @@
 use std::{fs::File, io::BufReader};
 use std::io::Read;
+use eframe::egui;
 
 pub mod bus;
 pub mod cpu;
+
+pub struct Ui();
+
+impl eframe::App for Ui {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        egui::CentralPanel::default().show(ctx, |ui| {
+            ui.heading("Hello world!");
+        });
+    }
+}
 
 fn main() {
     let f = File::open("example.bin").unwrap();
@@ -14,4 +25,5 @@ fn main() {
     let bus = bus::BasicBus::try_from(buffer).unwrap();
     let mut cpu6502 = cpu::Cpu::new(bus);
 
+    eframe::run_native("My egui app", eframe::NativeOptions::default(), Box::new(|_cc| Box::new(Ui{})));
 }
