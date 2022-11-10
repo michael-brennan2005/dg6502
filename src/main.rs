32898f0f62664d308e0abfda4b2b2a357679c041
Michael Brennan <micha@DESKTOP-POQ1R6E>
2022-11-09 22:06:22 -0700

    dash

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 use std::{fs::File, io::BufReader};
 use std::io::Read;
 use eframe::egui;
-
+use egui::{Window, TextEdit};
 pub mod bus;
 pub mod cpu;
 
@@ -9,9 +9,22 @@ pub struct Ui();
 
 impl eframe::App for Ui {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.heading("Hello world!");
-        });
+        let mut memory_view = String::new();
+        memory_view += "       00 01 02 03 04 05 06 07 08 09 0A 0B 0C 0D 0E 0F\n";
+        for i in 0..64 {
+            memory_view += format!("0x{:04X}\n", i * 16).as_str();
+        }
+        Window::new("Memory view")
+            .vscroll(true)
+            .resizable(true)
+            .default_height(300.0)
+            .show(ctx, |ui| {
+                ui.add_sized(ui.available_size(), TextEdit::multiline(&mut memory_view).font(egui::TextStyle::Monospace));
+            });
+    }
+
+    fn clear_color(&self, _visuals: &egui::Visuals) -> egui::Rgba {
+        egui::Rgba::from_rgb(0.1, 0.1, 0.1)
     }
 }
 
