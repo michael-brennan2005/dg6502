af35dc35e2211d4a2420ab97c5a3fd6fbebe934b
Tech0tron <tech0tron.roblox@gmail.com>
2023-03-31 11:31:52 -0600

    nestest shit

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,36 @@
-use std::{fs::File, io::{BufReader, Read, Write, stdout}};
+use std::{fs::{File, self}, io::{BufReader, Read, Write, stdout}};
 
 use bus::BasicCPUMemory;
 use cpu::Cpu;
 use crossterm::{ExecutableCommand, style::Print, terminal::Clear, execute, event::{read, Event, KeyCode, KeyEventKind}};
+
+use crate::bus::CPUMemory;
 
 pub mod bus;
 pub mod cpu;
 mod cpu_tests;
 
 fn main() {
-    let memory = BasicCPUMemory::from_file(&"test_exe/6502_functional_test.bin".to_string()).unwrap();
-    let mut cpu = Cpu::new(memory);
+    let nestest = fs::read("test_rom/nestest.nes").unwrap();
 
-    cpu.program_counter = 0x400;
+    let mut memory_vec = vec![0; 65536];
+    let start = 0xC000;
+    let length = 16384;
+
+    for i in 0..length {
+        memory_vec[start + i] = nestest[0x10 + i];
+    }
+
+    let nestest_mem = BasicCPUMemory::try_from(memory_vec).unwrap();
+    let mut cpu = Cpu::new(nestest_mem);
+
+    cpu.program_counter = 0xC000;
     loop {
         execute!(
             stdout(),
             Clear(crossterm::terminal::ClearType::All),
             Print(&cpu),
+            Print(format!("{:#X} {:#X}\n", cpu.bus.read(0x2), cpu.bus.read(0x3))),
             Print("Z - 1 steps X - 100 steps C - 1K steps V - 1M steps\n")
         );
 
@@ -28,6 +41,21 @@ fn main() {
                         KeyCode::Char('z') => {
                             cpu.step();
                         },
+                        KeyCode::Char('x') => {
+                            for _ in 0..100 {
+                                cpu.step();
+                            }
+                        },
+                        KeyCode::Char('c') => {
+                            for _ in 0..1000 {
+                                cpu.step();
+                            }
+                        },
+                        KeyCode::Char('v') => {
+                            for _ in 0..1000000 {
+                                cpu.step();
+                            }
+                        }
                         _ => {}
                     }
                 }
